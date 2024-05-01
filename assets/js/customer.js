40fd07f1b868998d500e6aa243515a5226daae01
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-05-01 21:11:45 +0530

    Apply Styles

--- assets/js/customer.js
+++ assets/js/customer.js
@@ -1,5 +1,6 @@
 $('#customer-mng-section').css({display:'block'});
 $('#item-mng-section').css({display:'none'});
+$('#order-mng-section').css({display:'none'});
 
 
 //Customer click
@@ -8,6 +9,7 @@ $('#navCustomer').on('click',()=>{
 
     $('#customer-mng-section').css({display:'block'});
     $('#item-mng-section').css({display:'none'});
+    $('#order-mng-section').css({display:'none'});
 
 });
 
@@ -16,6 +18,17 @@ $('#navItem').on('click',()=>{
     console.log("item click")
 
     $('#customer-mng-section').css({display:'none'});
+    $('#order-mng-section').css({display:'none'});
     $('#item-mng-section').css({display:'block'});
+});
+
+
+//order click
+$('#navOrder').on('click',()=>{
+    console.log("order click")
+
+    $('#customer-mng-section').css({display:'none'});
+    $('#item-mng-section').css({display:'none'});
+    $('#order-mng-section').css({display:'block'});
 });
 
