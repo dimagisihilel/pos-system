2020184c5e2d24865c7031fd0d5eb9f8e3b9feec
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-06-04 15:13:46 +0530

    complete update and delete items

--- controller/itemController.js
+++ controller/itemController.js
@@ -55,3 +55,74 @@ $("#btn-add-item").on('click', () => {
 
     loadTable();
 });
+
+// Function to highlight the clicked row and store its index
+$("#item-tbl-body").on('click', 'tr', function () {
+
+    $("#item-tbl-body tr").removeClass("selected-row");
+
+    $(this).addClass("selected-row");
+
+    selectedRowIndex = $(this).index();
+
+    let itemId = $(this).find(".item-id-value").text();
+    let itemDesc = $(this).find(".item-description-value").text();
+    let itemPrice = $(this).find(".item-unitPrice-value").text();
+    let itemQty = $(this).find(".item-qty-value").text();
+
+    $("#itemEditId").val(itemId);
+    $("#itemEditDes").val(itemDesc);
+    $("#itemEditPrice").val(itemPrice);
+    $("#itemEditqty").val(itemQty);
+});
+
+
+function applySelectedRowStyle() {
+    $("#item-tbl-body tr").removeClass("selected-row");
+    $("#item-tbl-body tr:eq(" + selectedRowIndex + ")").addClass("selected-row");
+}
+
+$("#btn-update-item").on('click', () => {
+    if (selectedRowIndex === -1) {
+        // No row is selected, show an error message or handle it as per your requirement
+        return;
+    }
+
+    var updatedItemId = $("#itemEditId").val();
+    var updatedItemDesc = $("#itemEditDes").val();
+    var updatedItemPrice = $("#itemEditPrice").val();
+    var updatedItemQty = $("#itemEditqty").val();
+
+    items[selectedRowIndex].itemid = updatedItemId;
+    items[selectedRowIndex].description = updatedItemDesc;
+    items[selectedRowIndex].unitprice = updatedItemPrice;
+    items[selectedRowIndex].qty = updatedItemQty;
+
+    loadTable();
+
+    applySelectedRowStyle();
+
+    selectedRowIndex = -1;
+});
+
+// Delete customer function
+$("#btn-dlt-item").on('click', () => {
+    if (selectedRowIndex === -1) {
+        alert("No row selected.");
+        return;
+    }
+
+    if (!confirm("Are you sure you want to delete this Item?")) {
+        return;
+    }
+
+    items.splice(selectedRowIndex, 1);
+    loadTable();
+    selectedRowIndex = -1;
+});
+
+
+
+
+
+
